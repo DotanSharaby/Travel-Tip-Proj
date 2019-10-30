f3620536406f258f413ace962249892b8dca8886
kostagon <49278316+kostagon@users.noreply.github.com>
2019-10-30 16:18:11 +0200

    weather progress

--- js/main.js
+++ js/main.js
@@ -4,23 +4,27 @@ console.log('Main!');
 import locService from './services/loc.service.js';
 import mapService from './services/map.service.js';
 
+
 window.onload = () => {
-    let userPos;
+    
     mapService.initMap()
         .then(() => {
             locService.getPosition()
                 .then(pos => {
-                    let loc = { lat: pos.coords.latitude, lng: pos.coords.longitude }
+                    let loc = {
+                        lat: pos.coords.latitude,
+                        lng: pos.coords.longitude
+                    }
                     mapService.addMarker(loc, 'My Location');
                     console.log('User position is:', pos.coords);
-                    userPos = pos.coords;
+                    // weather obj should destructure it to a more relevant obj
+                    console.log(locService.connectToWeatherApi(loc.lat, loc.lng));
                 })
                 .catch(err => {
                     console.log('err!!!', err);
                 })
         })
         .catch(console.log('INIT MAP ERROR'));
-
     document.querySelector('.my-loc-btn').addEventListener('click', (ev) => {
         console.log('Aha!', ev.target);
         mapService.panTo(userPos.latitude, userPos.longitude);
--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,6 +1,7 @@
 'use strict';
 export default {
-    getPosition
+    getPosition,
+    connectToWeatherApi
 }
 
 function getPosition() {
@@ -9,4 +10,12 @@ function getPosition() {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
+}
+
+
+
+// const QUERY_STRING = `http://api.openweathermap.org/data/2.5/`;
+function connectToWeatherApi(lat, lng) {
+    const API_WEATHER_KEY = 'a560842ebbafec0e712e1aef0039cc5b';
+    return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${API_WEATHER_KEY}`)
 }
