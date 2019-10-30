2778ac243f3f78006bf1c18aea409a3ec27a7b05
DotanSharaby <dotan.sharaby@gmail.com>
2019-10-30 17:47:05 +0200

    html small changes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ import mapService from './services/map.service.js';
 
 
 window.onload = () => {
-    
+
     mapService.initMap()
         .then(() => {
             locService.getPosition()
@@ -15,11 +15,12 @@ window.onload = () => {
                         lat: pos.coords.latitude,
                         lng: pos.coords.longitude
                     }
+                    // console.log('User position is:', pos.coords);
                     mapService.addMarker(loc, 'My Location');
-                    console.log('User position is:', pos.coords);
+                    
                     // weather obj should destructure it to a more relevant obj
                     console.log(locService.connectToWeatherApi(loc.lat, loc.lng));
-                    
+                    addListeners(loc);
                 })
                 .catch(err => {
                     console.log('err!!!', err);
@@ -28,12 +29,16 @@ window.onload = () => {
         .catch(console.log('INIT MAP ERROR'));
 
     // EVENT LISTENERS
-    document.querySelector('.my-loc-btn').addEventListener('click', (ev) => {
-        // console.log('Aha!', ev.target);
-        mapService.panTo(userPos.latitude, userPos.longitude);
-    })
-    document.querySelector('.loc-input-btn').addEventListener('click', (ev) => {
-        var elLocInput = document.querySelector('.loc-input').value;
-        console.log(elLocInput);
-    })
+    function addListeners(loc) {
+        document.querySelector('.my-loc-btn').addEventListener('click', (ev) => {
+            // console.log('Aha!', ev.target);
+            mapService.panTo(loc.lat, loc.lng);
+        })
+
+        document.querySelector('.loc-input-btn').addEventListener('click', (ev) => {
+            var elLocInput = document.querySelector('.loc-input').value;
+            console.log(elLocInput);
+            mapService.panTo(loc.lat, loc.lng);
+        })
+    }
 }
