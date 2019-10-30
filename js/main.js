9da8ec23b28f729c4fc42c4ac0b1c41c93da781a
kostagon <49278316+kostagon@users.noreply.github.com>
2019-10-30 18:06:35 +0200

    weather render

--- js/main.js
+++ js/main.js
@@ -14,6 +14,25 @@ window.onload = () => {
                         lat: pos.coords.latitude,
                         lng: pos.coords.longitude
                     }
+                    mapService.addMarker(loc, 'My Location');
+                    console.log('User position is:', pos.coords);
+                    
+                    locService.connectToWeatherApi(loc.lat, loc.lng)
+                        .then(({
+                            data
+                        }) => {
+                            let res = {
+                                cityName: data.name,
+                                iconCode: data.weather[0].icon,
+                                desc: data.weather[0].description,
+                                minTemp: data.main.temp_min,
+                                maxTemp: data.main.temp_max,
+                                temp: data.main.temp,
+                                windSpeed: data.wind.speed,
+                                country: data.sys.country,
+                            }
+                            renderWeatherContainer(res);
+                        });
                     // console.log('User position is:', pos.coords);
                     mapService.addMarker(loc, 'My Location');
                     
@@ -26,6 +45,24 @@ window.onload = () => {
                 })
         })
         .catch(console.log('INIT MAP ERROR'));
+    document.querySelector('.my-loc-btn').addEventListener('click', (ev) => {
+        mapService.panTo(userPos.lat, userPos.lng);
+    })
+}
+
+function renderWeatherContainer(data) {
+    let iconUrl = `http://openweathermap.org/img/wn/${data.iconCode}@2x.png`;
+    const container = document.querySelector('.weather-container');
+    let strHtmls = `
+        <h3>Weather Today</h3>
+        <img src="${iconUrl}"></img>
+        <p>${data.cityName}, ${data.country}<img src="https://www.countryflags.io/${data.country}/flat/24.png"></p>
+        <p style="background-color: grey; display: inline;">${data.temp} &#176;C </p><p>`;
+    if (data.minTemp !== data.maxTemp) {
+        strHtmls += `temperature from ${data.minTemp} to ${data.maxTemp} &#176;C,`;
+    }
+    strHtmls += ` wind ${data.windSpeed}km/ph</p>`;
+    container.innerHTML = strHtmls;
 
     // EVENT LISTENERS
     function addListeners(loc) {
